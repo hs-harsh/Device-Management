2f29d0ae22cf126d2a79aacfed6156857b78f679
Harsh Kumar <harshkumar@Harshs-MacBook-Pro.local>
2024-02-04 11:26:11 +0530

    Bar code reader added

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+APP = ['Device_management_QR.py']
+DATA_FILES = []
+OPTIONS = {
+    'argv_emulation': True,
+    'packages': ['wx'],
+}
+
+setup(
+    app=APP,
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
+
+# pyinstaller --onefile Device_management_QR.py
